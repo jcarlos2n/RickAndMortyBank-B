afcea0e1893c15302e18f8fdab28e1e475629b7e
jcarlos2n <eljcarlos2n@gmail.com>
2022-12-21 00:09:47 +0100

    create connection to DB and user model

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const mongoose = require("mongoose");
 const PORT = 3001;
 const router = require("./router");
 const cors = require("cors");
+require("dotenv").config();
+
+const connectionString = `mongodb+srv://jcarlos2n:LBVAv8X0G4IPsVR7@cluster0.td2tswa.mongodb.net/?retryWrites=true&w=majority`;
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
+
+mongoose
+    .connect(connectionString)
+    .then(() => {
+        console.log('Database connected');
+    }).catch(err => {
+        console.error("ERROR:",err);
+    });
 
 app.listen(PORT, ()=>{
     console.log("Server is running on port", PORT);
