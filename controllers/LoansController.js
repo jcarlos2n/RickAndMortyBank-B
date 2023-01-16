9b253d3d3f77c45afeaa940c046c6ea25a4f1019
jcarlos2n <eljcarlos2n@gmail.com>
2023-01-16 11:07:02 +0100

    feat: add some changes to LOAN function

--- controllers/LoansController.js
+++ controllers/LoansController.js
@@ -2,6 +2,7 @@
 const Loan = require("../models/Loan");
 const Account = require("../models/Account");
 const Notice = require('../models/Notice');
+const { findByIdAndDelete } = require("../models/Loan");
 
 const LoansController = {};
 const actualDate = new Date().toUTCString();
@@ -13,14 +14,14 @@ LoansController.createNewLoan = async (req, res) => {
         const months = req.body.months;
         const account_id = req.body.account_id;
 
-        const userAccount = await Account.findOne({_id: account_id, user_id: userId})
-        
+        const userAccount = await Account.findOne({ _id: account_id, user_id: userId })
+
         if (quantity == "" || months == "" || account_id == "") {
-            return res.json({ success: false, message: "Please fill in the missing fields"})
+            return res.json({ success: false, message: "Please fill in the missing fields" })
         }
 
         if (!userAccount) {
-            return res.json({ success: false, message: "This account does not belong to this user"})
+            return res.json({ success: false, message: "This account does not belong to this user" })
 
         }
 
@@ -32,7 +33,7 @@ LoansController.createNewLoan = async (req, res) => {
         })
 
         const quantityF = `+${quantity}€`;
-        const concept = `Has pedido un préstamo de ${quantity}€, a ${months} meses, con una cuota mensual de ${quantity/months}€`;
+        const concept = `Has pedido un préstamo de ${quantity}€, a ${months} meses, con una cuota mensual de ${quantity / months}€`;
         const createNotice = await Notice.create({
             quantity: quantityF,
             concept,
@@ -41,7 +42,7 @@ LoansController.createNewLoan = async (req, res) => {
         });
 
         const sumQuantity = await Account.findByIdAndUpdate(account_id, { $inc: { "balance": quantity } }, { new: false, safe: true, upsert: true })
-        
+
         return res.json({ success: true, data: createLoan, account: sumQuantity })
     } catch (error) {
         return res.json({ success: false, error: error })
@@ -66,27 +67,45 @@ LoansController.payQuote = async (req, res) => {
         const account = await Account.findById(getLoan.account_id);
 
         if (!getLoan || !account) {
-            return res.json({ success: false, message: "Loan or Account don´t found"});
+            return res.json({ success: false, message: "Loan or Account don´t found" });
         }
 
-        const opQuantity = getLoan.quantity-getLoan.quota;
-        const opQuota = getLoan.months-1;
-        const updateLoan = { quantity : opQuantity, months: opQuota};
-        const opAccount = account.balance-getLoan.quota;
-        const updateAccount = { balance : opAccount};
+
+
+        const opQuantity = getLoan.quantity - getLoan.quota;
+        const opQuota = getLoan.months - 1;
+        const updateLoan = { quantity: opQuantity, months: opQuota };
+        const opAccount = account.balance - getLoan.quota;
+        const updateAccount = { balance: opAccount };
         const upLoan = await Loan.findByIdAndUpdate(id, updateLoan, { new: true, safe: true, upsert: true })
         const upAccount = await Account.findByIdAndUpdate(getLoan.account_id, updateAccount, { new: true, safe: true, upsert: true })
-        
-        const quantityF = `-${getLoan.quota}€`;
-        const concept = `Has pagado una cuota de ${getLoan.quota} de tu préstamo, ya solo te quedarian ${opQuota} meses por pagar`;
-        const createNotice = await Notice.create({
-            quantity: quantityF,
-            concept,
-            date: actualDate,
-            account_id: getLoan.account_id,
-        });
 
-        return res.json({ success: true, loan: upLoan, account: upAccount })
+        if (upLoan.quantity <= 0) {
+            console.log(upLoan.quantity)
+            const quantityF = `-${getLoan.quota}€`;
+            const concept = `Has finalizado todas las cuotas de este prestamo ${getLoan._id}.`;
+            const createNotice = await Notice.create({
+                quantity: quantityF,
+                concept,
+                date: actualDate,
+                account_id: getLoan.account_id,
+            });
+            const deleteLoan = await Loan.findByIdAndDelete(id);
+
+            return res.json({ succes: true, loan: upLoan, account: upAccount, data: deleteLoan})
+        } else {
+            
+            const quantityF = `-${getLoan.quota}€`;
+            const concept = `Has pagado una cuota de ${getLoan.quota} de tu préstamo, ya solo te quedarian ${opQuota} meses por pagar`;
+            const createNotice = await Notice.create({
+                quantity: quantityF,
+                concept,
+                date: actualDate,
+                account_id: getLoan.account_id,
+            });
+
+            return res.json({ success: true, loan: upLoan, account: upAccount })
+        }
     } catch (error) {
         return res.json({ success: false, error: error })
     }
